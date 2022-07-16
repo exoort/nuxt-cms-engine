3614af5144fe2d27d3e8b01b04af2bef8543966f
exoort <exortwot@gmail.com>
2022-07-16 02:56:49 +0200

    fixed types

--- lib/core/utils.d.ts
+++ lib/core/utils.d.ts
@@ -3,7 +3,7 @@ import {ICms, ICmsLayout, ICmsPage, ICmsRoute, ICmsStructure} from "../../types"
 declare class CmsEngineUtils {
   static getPageConfig(url: string, config: ICms): ICmsPage;
   static getRouteParams(url: string, cmsUrl: string): Record<string, string>;
-  static getDetectBy(url: string): string[];
+  static getDetectBy(url: string, mockDynamic?: boolean): string[];
   static getFullUrlParts (url: string, limit?: number): string[];
   static getDefaultPage(): ICmsPage;
   static getDefaultCmsFile(): ICms;
